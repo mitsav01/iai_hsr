e3cbd484757600ed0661f9e8981375b57877382b
maltehue <122607802+maltehue@users.noreply.github.com>
2025-11-24 15:14:54 +0100

    Update switch_to_velocity_controllers.launch.py
    update launch file

--- hsr_velocity_controller/launch/switch_to_velocity_controllers.launch.py
+++ hsr_velocity_controller/launch/switch_to_velocity_controllers.launch.py
@@ -5,32 +5,10 @@ from launch.substitutions import Command,LaunchConfiguration, PathJoinSubstituti
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-
-    description_file = LaunchConfiguration('description_file', default='hsrb4s.urdf.xacro')
-
-    robot_description_content = Command([
-        PathJoinSubstitution([FindExecutable(name='xacro')]), ' ',
-        PathJoinSubstitution([get_package_share_directory('hsr_description'), 'robots', description_file])
-    ])
-    robot_description = {'robot_description': robot_description_content}
-
     controller_yaml_file = os.path.join(
     get_package_share_directory('hsr_velocity_controller'),
     'config', 'my_controller_realtime_test.yaml'
 )
-
-    # ros2_control Node
-    ros2_control_node = Node(
-        package='controller_manager',
-        executable='ros2_control_node',
-        name='controller_manager',
-        parameters=[
-            robot_description,
-            controller_yaml_file,
-            {'use_sim_time': False}
-        ],
-        output='screen'
-    )
     # Unspawn existing trajectory controllers
     unspawn_controllers = Node(
         package='controller_manager',
@@ -52,7 +30,6 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        #ros2_control_node,
         unspawn_controllers,
         velocity_controller_spawner
     ])
